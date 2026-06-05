3d4e92c53b056a882ddfc89466b06c4234ccf36d
Riya Vachhani <riya.vachhani0001@gmail.com>
2026-06-05 11:10:55 +0530

    adding

--- 5.React/Weather_App/src/SearchBox.jsx
+++ 5.React/Weather_App/src/SearchBox.jsx
@@ -7,8 +7,8 @@ export default function SearchBox({updateInfo}) {
         let [city, setCity] = useState("");
         let [error, setError] = useState(false);
         const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-        const API_KEY = "e35bbf927cf14f268f07f0df81355403";
-
+        const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+        
         let getWeatherInfo = async () => {
             try{
                 let response = await fetch(
